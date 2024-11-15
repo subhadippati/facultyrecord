55183f595cb6161bf2b497dfc4ea46baed5401b4
Subhadip pati <121764315+subhadippati@users.noreply.github.com>
2024-11-16 00:33:44 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,21 +2,17 @@ Create VIEW recent_discussions AS
 select COUNT(*), dg.dtype,dg.group_title, dt.last_updated_date    #, COUNT(*) HAVING COUNT(*) = 10
 from discussion_group as dg, discussion_thread as dt 	
 where dg.group_id=dt.group_id  group by dt.last_updated_date;
-#dg.dtype='GROUP' or  dg.dtype='CLUB'or dg.dtype='COURSE'and 
+dg.dtype='GROUP' or  dg.dtype='CLUB'or dg.dtype='COURSE'and 
 
 select * from recent_discussions;
-
-
 
 create view recent_discussion_created as
 select COUNT(*), dg.dtype,dg.group_title, dt.creation_date    
 from discussion_group as dg, discussion_thread as dt 	
 where dg.group_id=dt.group_id  group by dt.creation_date;
-#HAVING COUNT(*)<=10
+HAVING COUNT(*)<=10
 
 select * from recent_discussion_created;
-
-
 
 select * from moderator;
 create view moderator_set_1 as
@@ -24,19 +20,15 @@ select m.udid, m.first_name, dg.group_title
 from `moderator` as m
 right join `discussion_group` as dg
 on dg.moderator_user_id1=m.udid
-where m.user_id IN (select m.user_id
-					from `moderator` as m, `mainuser` as mu
-                    where m.user_id=mu.user_name) order by m.udid;
+where m.user_id IN (select m.user_id from `moderator` as m, `mainuser` as mu where m.user_id=mu.user_name) order by m.udid;
                     
-#2nd set of Moderatorsand the groups the are member of.
+set of Moderatorsand the groups the are member of.
 create view moderator_set_2 as
 select m.udid, m.first_name, dg.group_title 
 from `moderator` as m
 right join `discussion_group` as dg
 on dg.moderator_user_id2=m.udid
-where m.user_id IN (select m.user_id
-					from `moderator` as m, `mainuser` as mu
-                    where m.user_id=mu.user_name) order by m.udid;
+where m.user_id IN (select m.user_id from `moderator` as m, `mainuser` as mu where m.user_id=mu.user_name) order by m.udid;
 				
 select * from moderator_set_1;
 select * from moderator_set_2;
@@ -47,13 +39,9 @@ create view most_comments as
 select COUNT(*),  dg.dtype ,c.title,dt.discussion_title
 from comments as c, discussion_group as dg, discussion_thread as dt
 where c.discussion_id=dt.discussion_id 
-and dt.discussion_title IN (select dt.discussion_title
-							  from discussion_thread as dt
-							  where dt.group_id=dg.group_id) group by dt.discussion_title;
+and dt.discussion_title IN (select dt.discussion_title from discussion_thread as dt where dt.group_id=dg.group_id) group by dt.discussion_title;
 
 select * from most_comments;
-
-
 
 create view books_interested as
 select COUNT(*), s.first_name, br.title
@@ -63,8 +51,6 @@ on br.id=sbrm.books_and_references_id
 where sbrm.student_id=s.udid group by br.title;
 
 select * from books_interested;
-
-
 
 create view gpa_faculty as
 select fcp.faculty_id, fcp.course_id, c.course_name, f.first_name,
